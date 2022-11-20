a032c6da0ed0ad235b210bd1dcc964ec0a6d0921
Denis Kitaev <denkitaev@yandex-team.ru>
2022-11-20 18:28:02 +0300

    move fixtures

--- gendiff/tests/test_gendiff.py
+++ gendiff/tests/test_gendiff.py
@@ -6,9 +6,9 @@ from gendiff import generate_diff
 def test_flat_json():
     fixtures_path = os.path.join(os.path.dirname(__file__), 'fixtures')
 
-    file1_path = os.path.join(fixtures_path, 'flat_file1.json')
-    file2_path = os.path.join(fixtures_path, 'flat_file2.json')
-    result_path = os.path.join(fixtures_path, 'flat_diff')
+    file1_path = os.path.join(fixtures_path, 'src/flat_file1.json')
+    file2_path = os.path.join(fixtures_path, 'src/flat_file2.json')
+    result_path = os.path.join(fixtures_path, 'result/flat_diff_stylish')
     with open(result_path) as f:
         expected_result = f.read()
     assert generate_diff(file1_path, file2_path) == expected_result
@@ -17,9 +17,9 @@ def test_flat_json():
 def test_flat_yaml():
     fixtures_path = os.path.join(os.path.dirname(__file__), 'fixtures')
 
-    file1_path = os.path.join(fixtures_path, 'flat_file1.yaml')
-    file2_path = os.path.join(fixtures_path, 'flat_file2.yaml')
-    result_path = os.path.join(fixtures_path, 'flat_diff')
+    file1_path = os.path.join(fixtures_path, 'src/flat_file1.yaml')
+    file2_path = os.path.join(fixtures_path, 'src/flat_file2.yaml')
+    result_path = os.path.join(fixtures_path, 'result/flat_diff_stylish')
     with open(result_path) as f:
         expected_result = f.read()
     assert generate_diff(file1_path, file2_path) == expected_result
@@ -28,9 +28,9 @@ def test_flat_yaml():
 def test_nested_json():
     fixtures_path = os.path.join(os.path.dirname(__file__), 'fixtures')
 
-    file1_path = os.path.join(fixtures_path, 'nested_file1.json')
-    file2_path = os.path.join(fixtures_path, 'nested_file2.json')
-    result_path = os.path.join(fixtures_path, 'nested_diff')
+    file1_path = os.path.join(fixtures_path, 'src/nested_file1.json')
+    file2_path = os.path.join(fixtures_path, 'src/nested_file2.json')
+    result_path = os.path.join(fixtures_path, 'result/nested_diff_stylish')
     with open(result_path) as f:
         expected_result = f.read().strip()
     assert generate_diff(file1_path, file2_path) == expected_result
@@ -39,9 +39,9 @@ def test_nested_json():
 def test_nested_yaml():
     fixtures_path = os.path.join(os.path.dirname(__file__), 'fixtures')
 
-    file1_path = os.path.join(fixtures_path, 'nested_file1.yaml')
-    file2_path = os.path.join(fixtures_path, 'nested_file2.yaml')
-    result_path = os.path.join(fixtures_path, 'nested_diff')
+    file1_path = os.path.join(fixtures_path, 'src/nested_file1.yaml')
+    file2_path = os.path.join(fixtures_path, 'src/nested_file2.yaml')
+    result_path = os.path.join(fixtures_path, 'result/nested_diff_stylish')
     with open(result_path) as f:
         expected_result = f.read().strip()
     assert generate_diff(file1_path, file2_path) == expected_result
